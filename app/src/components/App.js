78ad66283fc783072fa2d5c5309f305c5c75ac86
evanmc21 <emcandler21@gmail.com>
2018-10-24 12:44:37 -0400

    restructured app

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -3,88 +3,14 @@ import beybraids from './images/beybraids.jpg';
 import bey2 from './images/bey2.jpg';
 import bey3 from './images/bey3.jpg';
 import bey4 from './images/bey4.jpg';
-
+import Popup from './Popup';
 import Pictures from './Pictures';
+import dataSet from '../dataSet/dataSet';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    const dataSet = [
-      {
-        question: "How many #1 solo albums does Beyonce have?",
-        answers: [
-          "4", "5", "6", "8"
-        ],
-        correct: 2
-      }, {
-        question: "How many years has Beyonce been a professional recording artist?",
-        answers: [
-          "9", "21", "15", "7"
-        ],
-        correct: 1
-      }, {
-        question: "What is Beyonce's favorite number?",
-        answers: [
-          "4", "31", "7", "23"
-        ],
-        correct: 0
-      }, {
-        question: "What song did Beyonce perform at the 2010 Grammy Awards?",
-        answers: [
-          "Halo", "If I Were A Boy", "Sweet Dreams", "Bleeding Love"
-        ],
-        correct: 1
-      }, {
-        question: "In what year did Beyonce and Jay-Z get married?",
-        answers: [
-          "2011", "2012", "2008", "Who is Jay-Z?"
-        ],
-        correct: 2
-      }, {
-        question: "How many #1 singles does Beyonce have on the hot 100?",
-        answers: [
-          "8", "4", "7", "6", "10"
-        ],
-        correct: 3
-      }, {
-        question: "Who came up with the beat in Yonce?",
-        answers: [
-          "Timbaland", "Justin Timberlake", "Solange", "Andre 3000", "Sia"
-        ],
-        correct: 1
-      }, {
-        question: "Which artist inspired Beyonce to start performing?",
-        answers: [
-          "Michael Jackson", "Tina Turner", "Whitney Houston", "Prince", "Dolly Parton"
-        ],
-        correct: 0
-      }, {
-        question: "Beyonce performed with whom at the 2016 Country Music Awards?",
-        answers: [
-          "Faith Hill & Tim McGraw", "The Dixie Chicks", "Carrie Underwood", "Jason Aldean"
-        ],
-        correct: 1
-      }, {
-        question: "What was Destiny's Child's first hit single?",
-        answers: [
-          "Say My Name", "No, No, No", "Survivor", "Bills, Bills, Bills"
-        ],
-        correct: 1
-      },{
-        question: "What Beyonce song was Missy Elliot featured on?",
-        answers: [
-          "Yes", "Speechless", "Signs", "Daddy Lessons"
-        ],
-        correct: 2
-      },{
-        question: "What song was Frank Ocean credited on as a co-writer on the album, 4?",
-        answers: [
-          "I Miss You", "I Was Here", "Dance for You", "Rather Die Young"
-        ],
-        correct: 0
-      }
-    ]
     this.state = {
       current: 0,
       dataSet: dataSet,
